d000529f049f2fc774528a1c16d96dfd2b92954b
John Lowery <johnlowery@Macbook2019.local>
2020-04-05 13:02:40 -0500

    setup Livewire component

--- app/Http/Livewire/HomeSearch.php
+++ app/Http/Livewire/HomeSearch.php
@@ -7,10 +7,23 @@ use App\Component as AlpineComponent;
 
 class HomeSearch extends Component
 {
+    public $search;
+    public $activeCategories = [];
+    public $activeTags = [];
+
     public function render()
     {
+        $components = $this->searchComponents();
+        $facets = $components->pluck('category')->countBy()->sort();
+
         return view('livewire.home-search', [
-            'groups' => AlpineComponent::with('creator')->orderBy('category')->orderBy('created_at')->get()->groupBy('category')
+            'facets' => $facets,
+            'groups' => $components->sortBy('category')->sortBy('created_at')->groupBy('category')
         ]);
+    }
+
+    public function searchComponents()
+    {
+        return AlpineComponent::search($this->search)->with('creator')->get();
     }
 }
