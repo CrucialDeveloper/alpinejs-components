b712bb4e55a05bacefa617165f9cf20fb9c91a54
John Lowery <johnlowery@Macbook2019.local>
2020-04-05 15:02:28 -0500

    setup home page component

--- resources/views/livewire/home-search.blade.php
+++ resources/views/livewire/home-search.blade.php
@@ -26,46 +26,46 @@
                     @foreach ($components as $component)
                     <div
                         class="w-full p-4 {{!$loop->first ? 'mt-8' :''}} bg-white rounded shadow-md border-4 border-transparent hover:border-blue-800">
-                        <div class="mb-2">
+                        <div class="mb-2>
                             {{$component->summary}}
                         </div>
-                        <span class="px-2 py-1 text-xs bg-gray-200 rounded">{{$component->category}}</span>
-                        <div class="mt-4 text-sm">
+                        <span class="px-2 py-1 text-xs bg-gray-200 rounded ">{{$component->category}}</span>
+                            <div class="mt-4 text-sm ">
                             {{$component->description}}
                         </div>
-                        <div class="flex items-center justify-between mt-4">
-                            <div class="flex items-center mr-4">
-                                <div class="flex items-center justify-center w-8 h-8 mr-4 text-white rounded-full "
-                                    style=" background:{{$component->creator->background_color}}">
-                                    {{$component->creator->first_name[0]}}{{$component->creator->last_name[0]}}
-                                </div>
-                                <span class="text-sm">{{$component->creator->first_name}}
-                                    {{$component->creator->last_name}}</span>
-                            </div>
-
-                            <div class="flex items-center">
-                                @if($component->approved_at)
-                                <button
-                                    class="px-4 py-2 mr-4 border border-blue-700 rounded hover:bg-blue-700 hover:text-white">
-                                    View Preview
-                                </button>
-                                @else
-                                <div class="px-4 py-2 mr-4 bg-gray-200 border border-blue-700 rounded">
-                                    View Preview (Pending)
-                                </div>
-                                @endif
-
-                                <button
-                                    class="px-4 py-2 border border-blue-700 rounded hover:bg-blue-700 hover:text-white">
-                                    View Code
-                                </button>
-                            </div>
+                        <div class="flex items-center justify-between mt-4 ">
+                            <div class="flex items-center mr-4 ">
+                            <div class="flex items-center justify-center w-8 h-8 mr-4 text-white rounded-full "
+                                style="
+                            background:{{$component->creator->background_color}}">
+                            {{$component->creator->first_name[0]}}{{$component->creator->last_name[0]}}
                         </div>
+                        <span class="text-sm">{{$component->creator->first_name}}
+                            {{$component->creator->last_name}}</span>
                     </div>
-                    @endforeach
+
+                    <div class="flex items-center">
+                        @if($component->approved_at)
+                        <button
+                            class="px-4 py-2 mr-4 border border-blue-700 rounded hover:bg-blue-700 hover:text-white">
+                            View Preview
+                        </button>
+                        @else
+                        <div class="px-4 py-2 mr-4 bg-gray-200 border border-blue-700 rounded">
+                            View Preview (Pending)
+                        </div>
+                        @endif
+
+                        <button class="px-4 py-2 border border-blue-700 rounded hover:bg-blue-700 hover:text-white">
+                            View Code
+                        </button>
+                    </div>
                 </div>
             </div>
             @endforeach
         </div>
     </div>
+    @endforeach
+</div>
+</div>
 </div>
